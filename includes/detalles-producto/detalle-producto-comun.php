42a89b084caf7606f2dbaef8eaf010d1b75789ea
Admin <pablolires@gmail.com>
2022-01-05 13:23:33 -0300

    Se oculto la primer imagen de las paginas detalle-producto

--- includes/detalles-producto/detalle-producto-comun.php
+++ includes/detalles-producto/detalle-producto-comun.php
@@ -10,12 +10,12 @@
         <hr>  
       </div>
 
-      <img 
+      <!-- <img 
         class="img-fluid" 
-        src="img/productos/<?= $product['img'] ?>" 
-        alt="Producto linea - <?= $product['line_rs'] .'- Código: '. $product['code'] ?>"
-      >
-      <hr class="only_desktop">
+        src="img/productos/<?//= $product['img'] ?>" 
+        alt="Producto linea - <?//= $product['line_rs'] .'- Código: '. $product['code'] ?>"
+      > -->
+      <!-- <hr class="only_desktop"> -->
       <img 
         class="img-fluid" 
         src="img/productos/<?= $product['img_t'] ?>" 
