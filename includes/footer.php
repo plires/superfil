ab273c21f4c60d7f6d1d65dcc6cd8a49fdb09858
Pablo Lires <pablolires@gmail.com>
2023-04-27 10:34:14 -0300

    Cambios de numeros de whatsapp

--- includes/footer.php
+++ includes/footer.php
@@ -25,19 +25,10 @@
 					<li>
 						<a 
 					    class="transition" 
-					    href="https://api.whatsapp.com/send?phone=+541138889308&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20" 
+					    href="https://api.whatsapp.com/send?phone=+5491157972255&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20" 
 					    target="_blank"
 					    rel="noopener">
-					      Whatsapp: +54 9 11 3888-9308
-						</a>
-					</li>
-					<li>
-						<a 
-					    class="transition" 
-					    href="https://api.whatsapp.com/send?phone=+541158094444&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20" 
-					    target="_blank"
-					    rel="noopener">
-					      Whatsapp: +54 9 11 5809-4444
+					      Whatsapp: +54 9 11 5797-2255
 						</a>
 					</li>
 					<li>
--- includes/wapp.php
+++ includes/wapp.php
@@ -14,7 +14,7 @@
 
   <a 
     id="whatsapp_desktop" 
-    href="https://web.whatsapp.com/send?phone=+541158094444&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20" 
+    href="https://web.whatsapp.com/send?phone=+5491157972255&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20" 
     target="_blank" 
     rel="noopener"
     class="btn btn-wap">CHAT
@@ -32,7 +32,7 @@
   <a 
     id="whatsapp_mobile" 
     class="transition" 
-    href="https://api.whatsapp.com/send?phone=+541158094444&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20" 
+    href="https://api.whatsapp.com/send?phone=+5491157972255&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20" 
     target="_blank"
     rel="noopener">
       <h5>¡CONSULTÁ POR <br> WHATSAPP!</h5>
