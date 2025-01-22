54e0418bc7625791fd9de04c46461c8b01f882ae
Pablo Lires <pablolires@gmail.com>
2025-01-22 15:30:54 -0300

    modificacion por cambios en campo de base de datos

--- includes/detalles-producto/detalle-producto-listex.php
+++ includes/detalles-producto/detalle-producto-listex.php
@@ -5,34 +5,31 @@
     <div class="col-md-6 image">
 
       <div class="only_mobile">
-        <h4>Productos / <?= $lines_extra['h1'] ?>  / Código <?= $product['code'] ?></h4>
+        <h4>Productos / <?= $lines_extra['h1'] ?> / Código <?= $product['code'] ?></h4>
         <h2>código <?= $product['code'] ?></h2>
-        <hr>  
+        <hr>
       </div>
 
       <?php /*
       <img 
         class="img-fluid" 
-        src="img/productos/<?= $product['img'] ?>" 
-        alt="Producto linea - <?= $product['line_rs'] .'- Código: '. $product['code'] ?>"
+        src="img/productos/<?= $product['img'] ?>"
+      alt="Producto linea - <?= $product['line_rs'] .'- Código: '. $product['code'] ?>"
       >
       */ ?>
-     
+
       <!-- <hr class="only_desktop"> -->
-      
-      <img 
-        class="img-fluid" 
-        src="img/productos/<?= $product['img_t'] ?>" 
-        alt="Producto linea - <?= $product['line_rs'] .'- Código: '. $product['code'] ?>"
-      >
+
+      <img class="img-fluid" src="img/productos/<?= $product['img_t'] ?>"
+        alt="Producto linea - <?= $product['line_rs'] .'- Código: '. $product['code'] ?>">
     </div>
 
     <div class="col-md-6 datos datos_listex">
 
       <div class="only_desktop">
-        <h4>Productos / <?= $lines_extra['h1'] ?>  / Código <?= $product['code'] ?></h4>
+        <h4>Productos / <?= $lines_extra['h1'] ?> / Código <?= $product['code'] ?></h4>
         <h2>código <?= $product['code'] ?></h2>
-        <hr>  
+        <hr>
       </div>
 
       <div>
@@ -40,39 +37,39 @@
 
         <!-- Material -->
         <?php if ($product['material']): ?>
-          <p><span>Material:</span> <br><?= $product['material'] ?></p>
+        <p><span>Material:</span> <br><?= $product['material'] ?></p>
         <?php endif ?>
 
         <!-- Medidas -->
         <?php if ($product['measures']): ?>
-          <p><span>Medida:</span> <br><?= $product['measures'] ?></p>
+        <p><span>Medida:</span> <br><?= $product['measures'] ?></p>
         <?php endif ?>
 
         <!-- Descripcion -->
         <?php if ($product['description']): ?>
-          <p><span>Descripción:</span> <br><?= $product['description'] ?></p>
+        <p><span>Descripción:</span> <br><?= $product['description'] ?></p>
         <?php endif ?>
 
         <!-- Largo Standard -->
-        <?php if ($product['long']): ?>
-          <p><span>Largo Standard:</span> <br><?= $product['long'] ?></p>
+        <?php if ($product['longs']): ?>
+        <p><span>Largo Standard:</span> <br><?= $product['longs'] ?></p>
         <?php endif ?>
 
         <!-- Colores -->
         <?php if ($product['color']): ?>
-          <p><span>Colores:</span> <br><?= $product['color'] ?></p>
+        <p><span>Colores:</span> <br><?= $product['color'] ?></p>
         <?php endif ?>
 
         <!-- Usos y Aplicaciones -->
         <?php if ($product['applications']): ?>
-          <p><span>Usos y aplicaciones:</span> <br><?= $product['applications'] ?></p>
+        <p><span>Usos y aplicaciones:</span> <br><?= $product['applications'] ?></p>
         <?php endif ?>
 
         <!-- Usos y Aplicaciones -->
         <?php if ($product['observations']): ?>
-          <hr>
-          <h3>Observaciones:</h3>
-          <p><?= $product['observations'] ?></p>
+        <hr>
+        <h3>Observaciones:</h3>
+        <p><?= $product['observations'] ?></p>
         <?php endif ?>
       </div>
 
