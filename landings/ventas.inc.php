0414f20b0b9c273bcf77bc0d1bba48c7225db3ec
Pablo Lires <pablolires@gmail.com>
2025-09-05 16:50:58 -0300

    Update ventas.inc.php

--- landings/ventas.inc.php
+++ landings/ventas.inc.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //////////////////////////////////////////////////////////////////////
 ///Copias de los mensajes
@@ -12,42 +12,33 @@ define('NAME_BCC_MARTIN', 'Martin Calcagno');
 //////////////////////////////////////////////////////////////////////
 ///Casillas con datos
 //////////////////////////////////////////////////////////////////////
-define('BETIANA', array(
-    'betiana@superfil.com.ar', 
-    'Betiana', 
-    'https://api.whatsapp.com/send/?phone=5491157972255&text=Hola%20Betiana!%20Necesito%20hacer%20una%20consulta!&app_absent=0', 
-    '11-5797-2255'
-));
-
 define('NOELIA', array(
-    'noelia@superfil.com.ar', 
-    'Noelia', 
-    'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%20Noelia!%20Necesito%20hacer%20una%20consulta!&app_absent=0', 
+    'noelia@superfil.com.ar',
+    'Noelia',
+    'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%20Noelia!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
     '11-3888-9308'
 ));
 
 define('GISELA', array(
-    'gisela@superfil.com.ar', 
-    'Gisela', 
-    'https://api.whatsapp.com/send/?phone=5491158094444&text=Hola%20Gisela!%20Necesito%20hacer%20una%20consulta!&app_absent=0', 
+    'gisela@superfil.com.ar',
+    'Gisela',
+    'https://api.whatsapp.com/send/?phone=5491158094444&text=Hola%20Gisela!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
     '11-5809-4444'
 ));
 
 define('MARTIN_C', array(
-    'martin@superfil.com.ar', 
-    'Martin', 
-    'https://api.whatsapp.com/send/?phone=5491131861400&text=Hola%20Martin!%20Necesito%20hacer%20una%20consulta!&app_absent=0', 
+    'martin@superfil.com.ar',
+    'Martin',
+    'https://api.whatsapp.com/send/?phone=5491131861400&text=Hola%20Martin!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
     '11-3186-1400'
 ));
 
 //////////////////////////////////////////////////////////////////////
 /// Rotatividad de casillas segun rubros
 //////////////////////////////////////////////////////////////////////
-define('EMAIL_VENTAS_CIELORRASOS', array( GISELA ));
-define('EMAIL_VENTAS_JUNTAS', array( BETIANA ));
-define('EMAIL_VENTAS_PALLETS', array( BETIANA ));
-define('EMAIL_VENTAS_TUBOS', array( BETIANA ));
-define('EMAIL_VENTAS_BANOS', array( MARTIN_C ));
-define('EMAIL_DEFAULT', array( MARTIN_C ));
-
-?>
+define('EMAIL_VENTAS_CIELORRASOS', array(GISELA));
+define('EMAIL_VENTAS_JUNTAS', array(NOELIA));
+define('EMAIL_VENTAS_PALLETS', array(NOELIA));
+define('EMAIL_VENTAS_TUBOS', array(NOELIA));
+define('EMAIL_VENTAS_BANOS', array(MARTIN_C));
+define('EMAIL_DEFAULT', array(MARTIN_C));
