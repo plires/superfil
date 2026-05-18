4d5a29de6eaee947f27febab67d8ceb0ca8ab1db
Pablo Lires <pablolires@gmail.com>
2026-05-18 11:28:08 -0300

    Cambios en recepcion de ventas. se deja de hacer seguimiento de este archivo

--- landings/ventas.inc.php
+++ landings/ventas.inc.php
@@ -6,35 +6,28 @@
 //////////////////////////////////////////////////////////////////////
 ///Copias de los mensajes
 //////////////////////////////////////////////////////////////////////
-define('EMAIL_BCC_MARTIN', 'martin@superfil.com.ar');
+define('EMAIL_BCC_MARTIN', 'martin@cantofil.com.ar');
 define('NAME_BCC_MARTIN', 'Martin Calcagno');
 
 //////////////////////////////////////////////////////////////////////
 ///Casillas con datos
 //////////////////////////////////////////////////////////////////////
 define('NOELIA', array(
-    'noelia@superfil.com.ar',
+    'noelia@cantofil.com.ar',
     'Noelia',
     'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%20Noelia!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
     '11-3888-9308'
 ));
 
 define('GISELA', array(
-    'gisela@superfil.com.ar',
+    'gisela@cantofil.com.ar',
     'Gisela',
     'https://api.whatsapp.com/send/?phone=5491158094444&text=Hola%20Gisela!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
     '11-5809-4444'
 ));
 
-define('MANUEL', array(
-    'manuel@superfil.com.ar',
-    'Manuel',
-    'https://api.whatsapp.com/send/?phone=5491157972255&text=Hola%20Manuel!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
-    '11 5797-2255'
-));
-
 define('MARTIN_C', array(
-    'martin@superfil.com.ar',
+    'martin@cantofil.com.ar',
     'Martin',
     'https://api.whatsapp.com/send/?phone=5491131861400&text=Hola%20Martin!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
     '11-3186-1400'
@@ -43,9 +36,9 @@ define('MARTIN_C', array(
 //////////////////////////////////////////////////////////////////////
 /// Rotatividad de casillas segun rubros
 //////////////////////////////////////////////////////////////////////
-define('EMAIL_VENTAS_CIELORRASOS', array(GISELA, MANUEL));
-define('EMAIL_VENTAS_JUNTAS', array(NOELIA));
-define('EMAIL_VENTAS_PALLETS', array(NOELIA));
-define('EMAIL_VENTAS_TUBOS', array(NOELIA, MANUEL));
+define('EMAIL_VENTAS_CIELORRASOS', array(GISELA));
+define('EMAIL_VENTAS_JUNTAS', array(GISELA));
+define('EMAIL_VENTAS_PALLETS', array(GISELA));
+define('EMAIL_VENTAS_TUBOS', array(GISELA));
 define('EMAIL_VENTAS_BANOS', array(MARTIN_C));
 define('EMAIL_DEFAULT', array(MARTIN_C));
