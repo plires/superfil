289bd51ad34b40d2f46dd08c0d36ac82e64aa553
Pablo Lires <pablolires@gmail.com>
2026-07-27 14:10:05 -0300

    Update: Cambios en recepcion de ventas.

--- landings/ventas.inc.php
+++ landings/ventas.inc.php
@@ -12,33 +12,42 @@ define('NAME_BCC_MARTIN', 'Martin Calcagno');
 //////////////////////////////////////////////////////////////////////
 ///Casillas con datos
 //////////////////////////////////////////////////////////////////////
-define('NOELIA', array(
-    'noelia@cantofil.com.ar',
-    'Noelia',
-    'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%20Noelia!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
+define('YANINA', array(
+    'yanina@cantofil.com.ar',
+    'Yanina',
+    'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%20Yanina!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
     '11-3888-9308'
 ));
 
-define('GISELA', array(
-    'gisela@cantofil.com.ar',
-    'Gisela',
-    'https://api.whatsapp.com/send/?phone=5491158094444&text=Hola%20Gisela!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
-    '11-5809-4444'
+define('PRISCILA', array(
+    'priscila@cantofil.com.ar',
+    'Priscila',
+    'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%Priscila!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
+    '11-3888-9308'
+));
+
+define('NATALIA', array(
+    'natalia@cantofil.com.ar',
+    'Natalia',
+    'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%20Natalia!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
+    '11-3888-9308'
 ));
 
 define('MARTIN_C', array(
     'martin@cantofil.com.ar',
     'Martin',
-    'https://api.whatsapp.com/send/?phone=5491131861400&text=Hola%20Martin!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
-    '11-3186-1400'
+    'https://api.whatsapp.com/send/?phone=5491138889308&text=Hola%20Martin!%20Necesito%20hacer%20una%20consulta!&app_absent=0',
+    '11-3888-9308'
 ));
+
+
 
 //////////////////////////////////////////////////////////////////////
 /// Rotatividad de casillas segun rubros
 //////////////////////////////////////////////////////////////////////
-define('EMAIL_VENTAS_CIELORRASOS', array(GISELA));
-define('EMAIL_VENTAS_JUNTAS', array(GISELA));
-define('EMAIL_VENTAS_PALLETS', array(GISELA));
-define('EMAIL_VENTAS_TUBOS', array(GISELA));
+define('EMAIL_VENTAS_CIELORRASOS', array(YANINA, PRISCILA, NATALIA));
+define('EMAIL_VENTAS_JUNTAS', array(YANINA, PRISCILA, NATALIA));
+define('EMAIL_VENTAS_PALLETS', array(YANINA, PRISCILA, NATALIA));
+define('EMAIL_VENTAS_TUBOS', array(YANINA, PRISCILA, NATALIA));
 define('EMAIL_VENTAS_BANOS', array(MARTIN_C));
 define('EMAIL_DEFAULT', array(MARTIN_C));
